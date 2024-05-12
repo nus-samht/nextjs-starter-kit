c0c88028b8206ca01a0022ea5509b53e90eda541
SamHT <samht@nustechnology.com>
2024-05-12 22:19:34 +0700

    Implement Redux Persist

--- app/StoreProvider.tsx
+++ app/StoreProvider.tsx
@@ -1,15 +1,22 @@
 'use client'
 
-import { store } from '@/lib/store'
+import { persistedStore, store } from '@/lib/store'
 import type { ReactNode } from 'react'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 interface Props {
   readonly children: ReactNode
 }
 
 const StoreProvider = ({ children }: Props) => {
-  return <Provider store={store}>{children}</Provider>
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistedStore}>
+        {children}
+      </PersistGate>
+    </Provider>
+  )
 }
 
 export default StoreProvider
